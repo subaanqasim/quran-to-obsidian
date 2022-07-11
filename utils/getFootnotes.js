6e4d7cf6d5872832924617ddb7c8bd1175bd8e4f
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-07-11 11:22:42 +0100

    Added rate limiting for footnotes endpoint

--- utils/getFootnotes.js
+++ utils/getFootnotes.js
@@ -1,7 +1,12 @@
 import fetch from "node-fetch";
 import { NodeHtmlMarkdown } from "node-html-markdown";
+import Bottleneck from "bottleneck";
 
 const nhm = new NodeHtmlMarkdown();
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 1500,
+});
 
 const footnoteRefRegex = /(?<=\=)(.*?)(?=\>)/g;
 
@@ -15,25 +20,28 @@ export default function getFootnotes(translationText, quranFilePrefix) {
   let results = [];
 
   for (const [i, ref] of footnoteRefs.entries()) {
-    fetch(`https://api.qurancdn.com/api/qdc/foot_notes/${ref}`)
-      .then((res) => res.json())
-      .then((data) => {
-        let text = nhm.translate(data.foot_note.text).trim();
+    limiter.schedule(() => {
+      fetch(`https://api.qurancdn.com/api/qdc/foot_notes/${ref}`)
+        .then((res) => res.json())
+        .then((data) => {
+          let text = nhm.translate(data.foot_note.text).trim();
 
-        if (/^.*?\[(\d+):(\d+)\].*$/.test(text) === true) {
-          // match to get verse key(s)
-          const surahNum = [...text.matchAll(/\[(\d+)\:/g)][0][1];
-          const verseNum = [...text.matchAll(/\:(\d+)\]/g)][0][1];
+          // if verseKey (e.g. [2:25]) is present
+          if (/^.*?\[(\d+):(\d+)\].*$/.test(text) === true) {
+            // match to get verse key(s)
+            const surahNum = [...text.matchAll(/\[(\d+)\:/g)][0][1];
+            const verseNum = [...text.matchAll(/\:(\d+)\]/g)][0][1];
 
-          //replace href with obsidian wikilink
-          text = text.replace(
-            /\[(.*?)\)/,
-            `[[${quranFilePrefix}${surahNum} - ${verseNum}|${surahNum}:${verseNum}]]`
-          );
-        }
+            //replace href with obsidian wikilink
+            text = text.replace(
+              /\[(.*?)\)/,
+              `[[${quranFilePrefix}${surahNum} - ${verseNum}|${surahNum}:${verseNum}]]`
+            );
+          }
 
-        results = [...results, { footnoteNum: i + 1, ref, text }];
-      });
+          results = [...results, { footnoteNum: i + 1, ref, text }];
+        });
+    });
   }
 
   return results;
